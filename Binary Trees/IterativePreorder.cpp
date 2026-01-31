7d48ddf7f6f23bf0f4ed52f68a4c1844ccc68bda
RituSingh09 <ritusingh0924@gmail.com>
2026-01-31 23:37:35 +0530

    Traversal commit

---
+++ Binary Trees/IterativePreorder.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+int main(){
+    int n;
+    cin>>n;
+    if(n==0){
+        return 0;
+    }
+    string val;
+    cin>>val;
+    if(val=="NULL"){
+        return 0;
+    }
+    Node *root=new Node(stoi(val));
+    queue<Node*> q;
+    q.push(root);
+    int cnt=1;
+    while(cnt<n && !q.empty()){
+        Node *temp=q.front();
+        q.pop();
+        cin>>val;
+        if(val!="NULL"){
+            Node *leftnode=new Node(stoi(val));
+            temp->left=leftnode;
+            q.push(leftnode);
+        }
+        cnt++;
+        cin>>val;
+        if(val!="NULL"){
+            Node *rightnode=new Node(stoi(val));
+            temp->right=rightnode;
+            q.push(rightnode);
+        }
+        cnt++;
+    }
+    stack<Node*> st;
+    st.push(root);
+    while(!st.empty()){
+        Node *node=st.top();
+        st.pop();
+        cout<<node->data<<" ";
+        if(node->right){
+            st.push(node->right);
+        }
+        if(node->left){
+            st.push(node->left);
+        }
+        
+    }
+    return 0;
+
+}
